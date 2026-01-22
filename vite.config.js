89ed0576705d7e4fe8d1c78f04fcf0c2a82574f5
ZoeKang <jujuhwa@gmail.com>
2026-01-22 17:28:03 +0800

    修改vite config.

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:
-    process.env.NODE_ENV === "production" ? `/2025-vite-react-week3/` : "/",
+    process.env.NODE_ENV === "production" ? `/2025-vite-react-week4/` : "/",
 })
